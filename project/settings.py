a9085f547dcaa415c7b0d3ddf44d047b2a4770d8
M_Madkour <mahhmoudmadkour@gmail.com>
2024-01-08 18:37:42 +0200

    add job models

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My 5 apps
+    'job',
+    'blog',
+    'accounts',
+    'contact',
+    'home',
+
 ]
 
 MIDDLEWARE = [
